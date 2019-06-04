c2b14d81a5845c8387e281905ca339dc908fa587
Mariia <maria.bookvoed@gmail.com>
2019-06-04 16:19:09 -0400

    tests de codigo

--- test/validateAuth.spec.js
+++ test/validateAuth.spec.js
@@ -4,20 +4,22 @@ describe("validateEmail",()=>{
     it('deberia retornar false, si el correo es invalido',()=>{
         expect(validateEmail('¢∞correoinvalido###')).toBe(false);
     })
-})
-describe("validateEmail",()=>{
     it('deberia retornar true, si el correo es valido',()=>{
         expect(validateEmail('correo_valido@gmail.com')).toBe(true);
     })
 })
 
+
 describe("validateNewUser", ()=>{
 	it('deberia retornar false si uno de los parametros esta string vacio',()=>{
 		expect(validateNewUser('test@test.com','')).toBe(false);
 	})
-})
-describe("validateNewUser", ()=>{
+	it('deberia retornar false si longitud de contraseña es menor a 6', ()=>{
+		expect(validateNewUser('test@test.com','abc1')).toBe(false);
+	})
 	it('deberia retornar true si todos parametros estan validos',()=>{
 		expect(validateNewUser('test@test.com','password')).toBe(true);
 	})
+
 })
+
